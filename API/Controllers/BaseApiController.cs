1e1594b265fd0a5bb6e9b13b0d6434c1a824a910
maciej szostak <szostak14@gmail.com>
2022-04-07 14:51:13 +0200

    Add react app

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
 	[ApiController]
 	[Route("api/[controller]")]
+	[EnableCors("CorsPolicy")]
+
     public class BaseApiController : ControllerBase
     {
         
